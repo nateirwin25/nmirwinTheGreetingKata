48dddd557efce524e546489889b985bd443b9756
Nathan Irwin <nateirwin25@gmail.com>
2020-04-10 16:15:26 -0700

    Finished greetings.py

--- greetings.py
+++ greetings.py
@@ -1,2 +1,68 @@
+lowerCase = []
+upperCase = []
+names = []
+
 def greet(name):
-    return ""
+    names.clear()
+    upperCase.clear()
+    lowerCase.clear()
+
+    if(name == None):
+        return "Hello, my friend."
+
+    if (isinstance(name, list)):
+        parse_list(name)
+    else:
+        parse_string(name)
+    parse_names()
+    return return_string()
+
+def parse_list(nameList):
+    for i in nameList:
+        parse_string(i)
+
+def parse_string(string):
+    quote = string.find('"')
+    if(quote != -1):
+        string = eval(string)
+        names.append(string)
+    else:
+        names.extend(string.split(', '))
+
+def parse_names():
+    for i in names:
+        if i.isupper():
+            upperCase.append(i)
+        else:
+            lowerCase.append(i)
+
+
+def return_string():
+    result = ""
+
+    if len(lowerCase) > 0:
+        result += "Hello, "
+    for i in range(len(lowerCase)-1):
+        result += (lowerCase[i] + ", ")
+    if len(lowerCase) > 1:
+        result += "and "
+    if len(lowerCase) > 0:
+        result += lowerCase[-1]
+    if len(lowerCase) > 0:
+        result += "."
+
+    if len(lowerCase) > 0 and len(upperCase) > 0:
+        result += " AND "
+    if len(upperCase) > 0:
+        result += "HELLO, "
+    for i in range(len(upperCase)-1):
+        result += (upperCase[i] + ", ")
+    if len(upperCase) > 1:
+        result += "AND "
+    if len(upperCase) > 0:
+        result += upperCase[-1]
+    if len(upperCase) > 0:
+        result += "!"
+
+    return result
+    
--- test_greeting.py
+++ test_greeting.py
@@ -6,7 +6,7 @@ def test_greeting():
     name = "name"
     result = greet("name")
 
-    assert result == "Hello name."
+    assert result == "Hello, name."
 
 def test_stand_in():
     result = greet(None)
@@ -17,24 +17,24 @@ def test_shouting():
     unshout_name_1 = "test"
     unshout_name_2 = "Test"
 
-    assert greet(shout_name) == "HELLO TEST!"
-    assert greet(unshout_name_1) == "Hello test."
-    assert greet(unshout_name_2) == "Hello Test."
+    assert greet(shout_name) == "HELLO, TEST!"
+    assert greet(unshout_name_1) == "Hello, test."
+    assert greet(unshout_name_2) == "Hello, Test."
 
 def test_two_names():
     name_1 = "test1"
     name_2 = "test2"
 
-    assert greet([name_1, name_2]) == "Hello, test1 and test2."
+    assert greet([name_1, name_2]) == "Hello, test1, and test2."
 
 def test_multiple_names():
-    assert greet(["test1", "test2", "test3", "test4"]) == "Hello test1, test2, test3, test4."
+    assert greet(["test1", "test2", "test3", "test4"]) == "Hello, test1, test2, test3, and test4."
 
 def test_multiple_shout():
-    assert greet("test1", "test2", "TEST3") == "Hello test1 and test2. AND HELLO BRIAN!"
+    assert greet(["test1", "test2", "TEST3"]) == "Hello, test1, and test2. AND HELLO, TEST3!"
 
 def test_comma_string():
-    assert greet("test1", "test2, TEST3") == "Hello test1 and test2. AND HELLO TEST3"
+    assert greet(["test1", "test2, TEST3"]) == "Hello, test1, and test2. AND HELLO, TEST3!"
 
 def test_escaped_comma():
-    asser greet("test1\, test2") == "Hello test1, test2."
+    assert greet("\"test1, test2\"") == "Hello, test1, test2."
